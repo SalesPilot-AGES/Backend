f5e433b4531b4a4ed5dedb13f2d51d7927c305c5
Eduardo Monteiro Tavares <eduardo.tavares002@edu.pucrs.br>
2026-03-13 17:25:51 -0300

    feat: init spring boot + maven project

---
+++ src/test/java/com/salespilot/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salespilot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
