120eec3b4c1cbe26b352e760849f279f5e92d555
Pedro Henrique <pedro.castilhos@edu.pucrs.br>
2026-04-08 23:27:15 -0300

    fix: adjusting for 409 error

--- application/src/main/java/com/salespilot/api/application/exception/CnpjAlreadyExists.java
+++ application/src/main/java/com/salespilot/api/application/exception/CnpjAlreadyExists.java
@@ -1,5 +1,9 @@
 package com.salespilot.api.application.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class CnpjAlreadyExists extends RuntimeException {
 
     public CnpjAlreadyExists() {
