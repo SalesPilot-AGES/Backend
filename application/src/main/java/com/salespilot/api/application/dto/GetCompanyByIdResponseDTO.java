c7d9bb1653c66daedd14898ad556608e66021594
Arthur Tomazelli de Campos <arthurtomazelli22@gmail.com>
2026-04-12 18:48:30 -0300

    refactor: remove unused imports

--- application/src/main/java/com/salespilot/api/application/dto/GetCompanyByIdResponseDTO.java
+++ application/src/main/java/com/salespilot/api/application/dto/GetCompanyByIdResponseDTO.java
@@ -1,11 +1,9 @@
 package com.salespilot.api.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.salespilot.api.domain.entity.Collaborator;
 import com.salespilot.api.domain.enums.CompanyPlan;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 public record GetCompanyByIdResponseDTO(@JsonProperty("id") UUID id,
