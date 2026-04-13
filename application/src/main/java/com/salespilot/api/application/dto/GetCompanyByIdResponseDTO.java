b3f72710be9a4677e29699c909d16d6ee3b253e5
daniel.rodrigues <daniel.a001@edu.pucrs.br>
2026-04-12 21:16:39 -0300

    refactor: format

--- application/src/main/java/com/salespilot/api/application/dto/GetCompanyByIdResponseDTO.java
+++ application/src/main/java/com/salespilot/api/application/dto/GetCompanyByIdResponseDTO.java
@@ -6,10 +6,12 @@ import com.salespilot.api.domain.enums.CompanyPlan;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record GetCompanyByIdResponseDTO(@JsonProperty("id") UUID id,
-                                        @JsonProperty("name") String name,
-                                        @JsonProperty("tax_id") String taxId,
-                                        @JsonProperty("plan") CompanyPlan plan,
-                                        @JsonProperty("is_active") boolean active,
-                                        @JsonProperty("created_at") LocalDateTime createdAt) {
+public record GetCompanyByIdResponseDTO(
+        @JsonProperty("id") UUID id,
+        @JsonProperty("name") String name,
+        @JsonProperty("tax_id") String taxId,
+        @JsonProperty("plan") CompanyPlan plan,
+        @JsonProperty("is_active") boolean active,
+        @JsonProperty("created_at") LocalDateTime createdAt
+) {
 }
