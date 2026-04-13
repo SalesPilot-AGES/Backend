8247ed40fe7fc380f73cc93fb50f7b80e174a3ad
daniel.rodrigues <daniel.a001@edu.pucrs.br>
2026-04-13 11:12:28 -0300

    refactor: move HTTP concerns from application to presentation layer
    - Move CompanyRequestDTO (with validation annotations) to presentation/dto
    - Remove @ResponseStatus from TaxIdAlreadyExists, add GlobalExceptionHandler
    - Remove jakarta.validation-api and spring-web from application/pom.xml
    - Add jakarta.validation-api to presentation/pom.xml

--- application/src/main/java/com/salespilot/api/application/exception/TaxIdAlreadyExists.java
+++ application/src/main/java/com/salespilot/api/application/exception/TaxIdAlreadyExists.java
@@ -1,9 +1,5 @@
 package com.salespilot.api.application.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class TaxIdAlreadyExists extends RuntimeException {
 
     public TaxIdAlreadyExists() {
--- presentation/src/main/java/com/salespilot/api/presentation/controller/CompanyController.java
+++ presentation/src/main/java/com/salespilot/api/presentation/controller/CompanyController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.salespilot.api.application.dto.CompanyRequestDTO;
 import com.salespilot.api.application.dto.CompanyResponseDTO;
+import com.salespilot.api.presentation.dto.CompanyRequestDTO;
 import com.salespilot.api.application.usecase.PostCompanyUseCase;
 
 import jakarta.validation.Valid;
