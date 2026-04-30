5e356ae4502f618a903071441845237cd5f7fafe
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2026-04-30 15:45:50 -0300

    feat: add initial configuration and methods to get all meetings count

---
+++ infrastructure/src/main/java/com/salespilot/api/infrastructure/persistence/jpa/repository/MeetingRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.salespilot.api.infrastructure.persistence.jpa.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.salespilot.api.domain.entity.Meeting;
+import com.salespilot.api.domain.repository.MeetingRepository;
+import com.salespilot.api.infrastructure.persistence.jpa.entity.Meetings;
+import com.salespilot.api.infrastructure.persistence.jpa.specification.MeetingSpecification;
+
+public class MeetingRepositoryImpl implements MeetingRepository{
+    private final MeetingsJpaRepository meetingsJpaRepository;
+
+    public MeetingRepositoryImpl(MeetingsJpaRepository meetingsJpaRepository) {
+        this.meetingsJpaRepository = meetingsJpaRepository;
+    }
+
+    public long getTotalMeetings(UUID collaboratorId) {
+        Specification<Meetings> spec = Specification.where(
+            MeetingSpecification.collaboratorIdEquals(collaboratorId)
+        );
+        return meetingsJpaRepository.count(spec);
+    }
+
+    @Override
+    public Optional<Meeting> findById(UUID id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Meeting> findByCollaboratorId(UUID collaboratorId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByCollaboratorId'");
+    }
+
+    @Override
+    public List<Meeting> findByClientId(UUID clientId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByClientId'");
+    }
+
+    @Override
+    public Meeting save(Meeting meeting) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+    
+}
--- infrastructure/src/main/java/com/salespilot/api/infrastructure/persistence/jpa/repository/MeetingsJpaRepository.java
+++ infrastructure/src/main/java/com/salespilot/api/infrastructure/persistence/jpa/repository/MeetingsJpaRepository.java
@@ -3,6 +3,7 @@ package com.salespilot.api.infrastructure.persistence.jpa.repository;
 import com.salespilot.api.infrastructure.persistence.jpa.entity.Meetings;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MeetingsJpaRepository extends JpaRepository<Meetings, UUID> {
+public interface MeetingsJpaRepository extends JpaRepository<Meetings, UUID>, JpaSpecificationExecutor<Meetings>{
 }
