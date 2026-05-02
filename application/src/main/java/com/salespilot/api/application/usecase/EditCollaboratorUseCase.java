7b8d302eb896810e8fc6d375f8aa37fb8abf9c05
Pedro Henrique <pedro.castilhos@edu.pucrs.br>
2026-05-02 18:57:39 -0300

    refactor: Adapting the code to only one Use Case for both endpoints of edit collaborator

--- application/src/main/java/com/salespilot/api/application/usecase/EditCollaboratorUseCase.java
+++ application/src/main/java/com/salespilot/api/application/usecase/EditCollaboratorUseCase.java
@@ -7,7 +7,9 @@ import com.salespilot.api.application.dto.CompanyResponseDTO;
 import com.salespilot.api.application.exception.CollaboratorAlreadyExistsException;
 import com.salespilot.api.application.exception.CollaboratorNotFoundException;
 import com.salespilot.api.application.exception.CompanyNotFoundException;
+import com.salespilot.api.application.exception.InvalidCollaboratorRoleException;
 import com.salespilot.api.domain.entity.Collaborator;
+import com.salespilot.api.domain.enums.CollaboratorRole;
 import com.salespilot.api.domain.repository.CollaboratorRepository;
 import com.salespilot.api.domain.repository.CompanyRepository;
 import com.salespilot.api.domain.valueobject.CollaboratorPreferences;
@@ -21,7 +23,7 @@ public class EditCollaboratorUseCase {
         this.companyRepository = companyRepository;
     }
 
-    public CollaboratorResponseDTO execute(UUID companyId, UUID collaboratorId, String name, String email, String phone, boolean active, CollaboratorPreferences collaboratorPreferences){
+    public CollaboratorResponseDTO execute(UUID companyId, UUID collaboratorId, String name, String email, String phone, boolean active, CollaboratorPreferences collaboratorPreferences, CollaboratorRole role){
         CompanyResponseDTO companyDto = companyRepository.getCompanyById(companyId)
                 .map(CompanyResponseDTO::from)
                 .orElseThrow(() -> new CompanyNotFoundException(companyId));
@@ -29,8 +31,15 @@ public class EditCollaboratorUseCase {
         Collaborator existing = repository.getCollaboratorById(collaboratorId)
                 .orElseThrow(() -> new CollaboratorNotFoundException(collaboratorId));
 
-        if (!existing.getEmail().equals(email) && repository.existsByCompanyIdAndEmail(companyId, email)) {
-            throw new CollaboratorAlreadyExistsException(companyId, email);
+        if(existing.getRole() != role) {
+            throw new InvalidCollaboratorRoleException(existing.getRole(), role);
+        }
+
+        boolean companyChanged = !existing.getCompanyId().equals(companyId);
+        boolean emailChanged = !existing.getEmail().equals(email);
+
+        if ((companyChanged || emailChanged) && repository.existsByCompanyIdAndEmail(companyId, email)) {
+             throw new CollaboratorAlreadyExistsException(companyId, email);
         }
 
         Collaborator collaborator = repository.update(companyId, collaboratorId, name, email, phone, active, collaboratorPreferences);
