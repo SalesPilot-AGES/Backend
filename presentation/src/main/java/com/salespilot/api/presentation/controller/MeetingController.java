91fcaf6fdc40aac4b713399eb8ef8d2aafb0236c
Eduardo Monteiro Tavares <eduardo.tavares002@edu.pucrs.br>
2026-05-02 09:30:29 -0300

    minor fix: imports

--- presentation/src/main/java/com/salespilot/api/presentation/controller/MeetingController.java
+++ presentation/src/main/java/com/salespilot/api/presentation/controller/MeetingController.java
@@ -8,7 +8,11 @@ import com.salespilot.api.presentation.utils.PageableUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
