ea235d6667a2fa9fbb51084950af2260e165d3f0
Pedro Henrique <pedro.castilhos@edu.pucrs.br>
2026-05-19 18:04:45 -0300

    refactor: changing the period validation to DashboardPeriodUtils

--- application/src/main/java/com/salespilot/api/application/usecase/GetCardMetricsUseCase.java
+++ application/src/main/java/com/salespilot/api/application/usecase/GetCardMetricsUseCase.java
@@ -2,12 +2,10 @@ package com.salespilot.api.application.usecase;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 
 import com.salespilot.api.application.dto.CardMetricsResponseDTO;
 import com.salespilot.api.application.dto.GroupCardMetricsResponseDTO;
-import com.salespilot.api.application.exception.InvalidPeriodException;
+import com.salespilot.api.application.utils.DashboardPeriodUtils;
 import com.salespilot.api.domain.enums.MetricsTrends;
 import com.salespilot.api.domain.repository.CollaboratorRepository;
 import com.salespilot.api.domain.repository.CompanyRepository;
@@ -25,69 +23,12 @@ public class GetCardMetricsUseCase {
     }
 
     public GroupCardMetricsResponseDTO execute(String period, LocalDate startDate, LocalDate endDate) {
-        LocalDateTime currentStart;
-        LocalDateTime currentEnd;
-        LocalDateTime previousStart;
-        LocalDateTime previousEnd;
-        LocalDate today = LocalDate.now();
-        LocalDate previousMonth = today.minusMonths(1);
-
-        switch (period) {
-            case "7d":
-                currentStart = today
-                    .minusDays(6)
-                    .atStartOfDay();
-                currentEnd = today.atTime(LocalTime.MAX);
-                previousStart = currentStart.minusDays(7);
-                previousEnd = currentEnd.minusDays(7);
-                break;
-            case "30d":      
-                currentStart = today
-                    .minusDays(29)
-                    .atStartOfDay();
-                currentEnd = today.atTime(LocalTime.MAX);
-                previousStart = currentStart.minusDays(30);
-                previousEnd = currentEnd.minusDays(30);
-                break;
-            case "all":
-                currentStart = today
-                    .withDayOfMonth(1)
-                    .atStartOfDay();
-                currentEnd = today.atTime(LocalTime.MAX);  
-                previousStart = previousMonth
-                        .withDayOfMonth(1)
-                        .atStartOfDay();
-                int previousDay =
-                        Math.min(
-                                today.getDayOfMonth(),
-                                previousMonth.lengthOfMonth()
-                        );
-                previousEnd = previousMonth
-                        .withDayOfMonth(previousDay)
-                        .atTime(LocalTime.MAX);
-
-                break;
-            case "custom":
-                if (startDate == null || endDate == null) {
-                    throw new InvalidPeriodException(period);
-                }
-
-                if (startDate.isAfter(endDate)) {
-                    throw new InvalidPeriodException(period);
-                }
-
-                currentStart = startDate.atStartOfDay();
-                currentEnd = endDate.atTime(LocalTime.MAX);
-
-                long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
-
-                previousStart = currentStart.minusDays(days);
-                previousEnd = currentEnd.minusDays(days);
-                break;
-            default:
-                throw new InvalidPeriodException(period);
-        }
-
+        LocalDateTime[] dates = DashboardPeriodUtils.dashboardPeriodUtilsToCardMetrics(period, startDate, endDate);
+        LocalDateTime previousStart = dates[0];
+        LocalDateTime previousEnd = dates[1];
+        LocalDateTime currentStart = dates[2];
+        LocalDateTime currentEnd = dates[3];
+        
         Long currentActiveCompanies = companyRepository.countCompaniesByActiveValue(true);
         Long previousActiveCompanies = companyRepository.countCompaniesByActiveValueAndPeriod(true, previousEnd);
 
---
+++ application/src/main/java/com/salespilot/api/application/utils/DashboardPeriodUtils.java
@@ -0,0 +1,77 @@
+package com.salespilot.api.application.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import com.salespilot.api.application.exception.InvalidPeriodException;
+
+public class DashboardPeriodUtils {
+    public static LocalDateTime[] dashboardPeriodUtilsToCardMetrics(String period, LocalDate startDate, LocalDate endDate) {
+        LocalDateTime previousEnd;
+        LocalDateTime previousStart;
+        LocalDateTime currentStart;
+        LocalDateTime currentEnd;
+        LocalDate today = LocalDate.now();
+        LocalDate previousMonth = today.minusMonths(1);
+
+        switch (period) {
+            case "7d":
+                currentStart = today
+                    .minusDays(6)
+                    .atStartOfDay();
+                currentEnd = today.atTime(LocalTime.MAX);
+                previousStart = currentStart.minusDays(7);
+                previousEnd = currentEnd.minusDays(7);
+                break;
+            case "30d":      
+                currentStart = today
+                    .minusDays(29)
+                    .atStartOfDay();
+                currentEnd = today.atTime(LocalTime.MAX);
+                previousStart = currentStart.minusDays(30);
+                previousEnd = currentEnd.minusDays(30);
+                break;
+            case "all":
+                currentStart = today
+                    .withDayOfMonth(1)
+                    .atStartOfDay();
+                currentEnd = today.atTime(LocalTime.MAX);  
+                previousStart = previousMonth
+                        .withDayOfMonth(1)
+                        .atStartOfDay();
+                int previousDay =
+                        Math.min(
+                                today.getDayOfMonth(),
+                                previousMonth.lengthOfMonth()
+                        );
+                previousEnd = previousMonth
+                        .withDayOfMonth(previousDay)
+                        .atTime(LocalTime.MAX);
+
+                break;
+            case "custom":
+                if (startDate == null || endDate == null) {
+                    throw new InvalidPeriodException(period);
+                }
+
+                if (startDate.isAfter(endDate)) {
+                    throw new InvalidPeriodException(period);
+                }
+
+                currentStart = startDate.atStartOfDay();
+                currentEnd = endDate.atTime(LocalTime.MAX);
+
+                long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
+
+                previousStart = currentStart.minusDays(days);
+                previousEnd = currentEnd.minusDays(days);
+                break;
+            default:
+                throw new InvalidPeriodException(period);
+        }
+
+        return new LocalDateTime[] { previousStart, previousEnd, currentStart, currentEnd };
+    }
+}
