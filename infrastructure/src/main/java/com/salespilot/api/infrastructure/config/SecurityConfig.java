74afc983e9ee847137b7bd8ec11453db272432b0
KillianDB <killiandomingues57@gmail.com>
2026-05-28 13:58:23 -0300

    fix: cors

--- infrastructure/src/main/java/com/salespilot/api/infrastructure/config/SecurityConfig.java
+++ infrastructure/src/main/java/com/salespilot/api/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.salespilot.api.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,8 +16,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/actuator/health",
